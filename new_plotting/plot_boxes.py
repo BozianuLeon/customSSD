e61f4236a9f8ee6595205690ebae670707807157
bozianuleon <74475015+BozianuLeon@users.noreply.github.com>
2023-10-05 17:09:38 +0200

    more box plots, w/ binned sep.

--- new_plotting/plot_boxes.py
+++ new_plotting/plot_boxes.py
@@ -118,7 +118,7 @@ def make_box_metric_plots(
     plt.close()
 
     #####################################################################################################################################
-    #Plot 2, the number of matched predicted and true boxes
+    #Plot 2, the number of unmatched predicted and true boxes
     n_unmatch_truth = load_object(folder_containing_lists + '/n_unmatched_truth.pkl')
     n_unmatch_preds = load_object(folder_containing_lists + '/n_unmatched_preds.pkl')
 
@@ -139,7 +139,7 @@ def make_box_metric_plots(
     plt.close()
 
     #####################################################################################################################################
-    #Plot 2b, the number of matched predicted and true boxes
+    #Plot 2b, the number of unmatched predicted and true boxes
     f,ax = plt.subplots(2,1,figsize=(8, 6), sharex=True, gridspec_kw={'height_ratios': [4, 1]})
     freq_tru, bins, _ = ax[0].hist(n_unmatch_truth,bins=50,range=(0,max(n_unmatch_truth)),density=True,histtype='step',color='green',label='Truth {:.2f}$\pm${:.1f}'.format(np.mean(n_unmatch_truth),np.std(n_unmatch_truth)))
     freq_pred, _, _ = ax[0].hist(n_unmatch_preds,bins=bins,density=True,histtype='step',color='red',label='Predictions {:.2f}$\pm${:.1f}'.format(np.mean(n_unmatch_preds),np.std(n_unmatch_preds)))
@@ -157,7 +157,7 @@ def make_box_metric_plots(
     plt.close()
 
     #####################################################################################################################################
-    #Plot 3, the number of unmatched predicted and true boxes
+    #Plot 3, the number of matched predicted and true boxes
     n_match_truth = load_object(folder_containing_lists + '/n_matched_truth.pkl')
     n_match_preds = load_object(folder_containing_lists + '/n_matched_preds.pkl')
 
@@ -178,7 +178,7 @@ def make_box_metric_plots(
     plt.close()
 
     #####################################################################################################################################
-    #Plot 3b, the number of unmatched predicted and true boxes
+    #Plot 3b, the number of matched predicted and true boxes
     f,ax = plt.subplots(2,1,figsize=(8, 6), sharex=True, gridspec_kw={'height_ratios': [4, 1]})
     freq_tru, bins, _ = ax[0].hist(n_match_truth,bins=50,range=(0,max(n_match_truth)),density=True,histtype='step',color='green',label='Truth {:.2f}$\pm${:.1f}'.format(np.mean(n_match_truth),np.std(n_match_truth)))
     freq_pred, _, _ = ax[0].hist(n_match_preds,bins=bins,density=True,histtype='step',color='red',label='Predictions {:.2f}$\pm${:.1f}'.format(np.mean(n_match_preds),np.std(n_match_preds)))
@@ -236,6 +236,91 @@ def make_box_metric_plots(
     plt.close()
 
 
+    #####################################################################################################################################
+    #plot 5 number of unmatched boxes when n_truth boxes <50
+    #####################################################################################################################################
+    #Binned PLots
+    bind_save_loc = save_loc + "binned"
+    if not os.path.exists(bind_save_loc):
+        os.makedirs(bind_save_loc)    
+    
+    n_truth_arr = np.array(n_truth)
+    n_unmatch_truth_arr = np.array(n_unmatch_truth)
+    n_unmatch_preds_arr = np.array(n_unmatch_preds)
+    binnings = [0,30,50,80,100,200]
+    numbers_of_bins = [15,25,35,40,50]
+    for i in range(len(binnings)-1):
+        # clusters_mask = n_truth_arr > binnings[i]
+        clusters_mask = (n_truth_arr > binnings[i]) & (n_truth_arr < binnings[i+1])
+
+        n_unm_truth_many = n_unmatch_truth_arr[clusters_mask]
+        n_unm_preds_many = n_unmatch_preds_arr[clusters_mask]
+
+        f,ax = plt.subplots(2,1,figsize=(8, 6), sharex=True, gridspec_kw={'height_ratios': [4, 1]})
+        freq_tru, bins, _ = ax[0].hist(n_unm_truth_many,bins=numbers_of_bins[i],range=(0,max(n_unm_truth_many)),histtype='step',color='green',label='Truth {:.2f}$\pm${:.1f}'.format(np.mean(n_unm_truth_many),np.std(n_unm_truth_many)))
+        freq_pred, _, _ = ax[0].hist(n_unm_preds_many,bins=bins,histtype='step',color='red',label='Predictions {:.2f}$\pm${:.1f}'.format(np.mean(n_unm_preds_many),np.std(n_unm_preds_many)))
+        ax[0].grid()
+        ax[0].set(ylabel='Freq.',title='{} Events, [{},{}] true objects '.format(len(n_unm_truth_many),binnings[i],binnings[i+1]),yscale='log')
+        ax[0].legend()
+
+        bin_centers = (bins[:-1] + bins[1:]) / 2
+        ax[1].scatter(bin_centers, get_ratio(freq_pred,freq_tru),marker='_',color='red',s=50)
+        ax[1].axhline(1,ls='--',color='green',alpha=0.5)
+        ax[1].set(xlabel="Number of Unmatched Boxes (per event)",ylabel='Ratio')
+        ax[1].grid()
+        f.subplots_adjust(hspace=0)
+        f.savefig(bind_save_loc + '/unmatched_n_boxes_bin{}.png'.format(i))
+        plt.close()
+    
+    #####################################################################################################################################
+    #Binned PLots
+
+    n_match_truth_arr = np.array(n_match_truth)
+    n_match_preds_arr = np.array(n_match_preds)
+    binnings = [0,30,50,80,100,200]
+    numbers_of_bins = [15,25,35,40,50]
+    for i in range(len(binnings)-1):
+        clusters_mask = (n_truth_arr > binnings[i]) & (n_truth_arr < binnings[i+1])
+
+        n_m_truth_many = n_match_truth_arr[clusters_mask]
+        n_m_preds_many = n_match_preds_arr[clusters_mask]
+
+        f,ax = plt.subplots(2,1,figsize=(8, 6), sharex=True, gridspec_kw={'height_ratios': [4, 1]})
+        freq_tru, bins, _ = ax[0].hist(n_m_truth_many,bins=numbers_of_bins[i],range=(0,max(n_m_truth_many)),histtype='step',color='green',label='Truth {:.2f}$\pm${:.1f}'.format(np.mean(n_m_truth_many),np.std(n_m_truth_many)))
+        freq_pred, _, _ = ax[0].hist(n_m_preds_many,bins=bins,histtype='step',color='red',label='Predictions {:.2f}$\pm${:.1f}'.format(np.mean(n_m_preds_many),np.std(n_m_preds_many)))
+        ax[0].grid()
+        ax[0].set(ylabel='Freq.',title='{} Events, [{},{}] true objects '.format(len(n_m_truth_many),binnings[i],binnings[i+1]),yscale='log')
+        ax[0].legend()
+
+        bin_centers = (bins[:-1] + bins[1:]) / 2
+        ax[1].scatter(bin_centers, get_ratio(freq_pred,freq_tru),marker='_',color='red',s=50)
+        ax[1].axhline(1,ls='--',color='green',alpha=0.5)
+        ax[1].set(xlabel="Number of Matched Boxes (per event)",ylabel='Ratio')
+        ax[1].grid()
+        f.subplots_adjust(hspace=0)
+        f.savefig(bind_save_loc + '/matched_n_boxes_bin{}.png'.format(i))
+        plt.close()
+
+    #####################################################################################################################################
+    #Binned PLots
+
+    perc_truth_area = load_object(folder_containing_lists + '/percentage_truth_area_covered.pkl')
+    perc_truth_area_arr = np.array(perc_truth_area)
+    binnings = [0,30,50,80,100,200]
+
+    fig,ax = plt.subplots()
+    for i in range(len(binnings)-1):
+        clusters_mask = (n_truth_arr > binnings[i]) & (n_truth_arr < binnings[i+1])
+
+        mask_perc_truth_area_arr = perc_truth_area_arr[clusters_mask]
+        ax.hist(mask_perc_truth_area_arr,bins=40,range=(0,1),density=True,histtype='step',label='[{},{}] true objects, {} events'.format(binnings[i],binnings[i+1],len(mask_perc_truth_area_arr)))
+    
+    ax.hist(perc_truth_area_arr,bins=40,range=(0,1),density=True,histtype='step',lw=0.75,color='black',label='Overall {} events'.format(len(perc_truth_area_arr)))
+    ax.grid()
+    ax.set(xlabel='Percentage of matched truth boxes covered by predictions (%)',ylabel='Freq.')
+    ax.legend(fontsize='small', frameon=False)
+    fig.savefig(bind_save_loc + '/perc_truth_cover_bin{}.png'.format(i))
+    plt.close()
 
     
 
