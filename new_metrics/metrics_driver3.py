e518e1f8c77a44c23f0d7fc2822b91a32c22baab
bozianuleon <74475015+BozianuLeon@users.noreply.github.com>
2023-11-03 17:13:44 +0100

    point to shared space

--- new_metrics/metrics_driver3.py
+++ new_metrics/metrics_driver3.py
@@ -4,8 +4,8 @@ from make_jet_metrics import calculate_jet_metrics
 
 
 
-folder_to_look_in = "/home/users/b/bozianu/work/SSD/SSD/cached_inference/SSD_50k5_mu_20e/20231005-12/"
-save_at = "/home/users/b/bozianu/work/SSD/SSD/cached_metrics/SSD_50k5_mu_20e/"
+folder_to_look_in = "/home/users/b/bozianu/work/SSD/SSD/cached_inference/SSD1_50k5_mu_15e/20231102-13/"
+save_at = "/home/users/b/bozianu/work/SSD/SSD/cached_metrics/SSD1_50k5_mu_15e"
 
 
 if __name__=="__main__":
