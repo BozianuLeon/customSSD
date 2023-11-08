74eaa3a19d8f0c49220ed6f769a3499ed0c11a4a
bozianuleon <74475015+BozianuLeon@users.noreply.github.com>
2023-11-08 17:22:38 +0100

    repoint

--- new_metrics/metrics_driver2.py
+++ new_metrics/metrics_driver2.py
@@ -1,14 +1,14 @@
 # from make_box_metrics import calculate_box_metrics
-from make_phys_metrics import calculate_phys_metrics
+# from make_phys_metrics import calculate_phys_metrics
+from make_phys_metrics2 import calculate_phys_metrics2
 # from make_jet_metrics import calculate_jet_metrics
 
 folder_to_look_in = "/home/users/b/bozianu/work/SSD/SSD/cached_inference/SSD1_50k5_mu_15e/20231102-13/"
-save_at = "/home/users/b/bozianu/work/SSD/SSD/cached_metrics/SSD1_50k5_mu_15e"
-
+save_at = "/home/users/b/bozianu/work/SSD/SSD/cached_metrics/SSD1_50k5_mu_15e/"
 
 if __name__=="__main__":
     print('Making phys metrics')
-    calculate_phys_metrics(folder_to_look_in,save_at)
+    calculate_phys_metrics2(folder_to_look_in,save_at)
     print('Completed [phys] metrics\n')
 
 
