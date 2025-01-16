d14ef61f31324069e1144fc78b88c49d09be519e
bozianuleon <74475015+BozianuLeon@users.noreply.github.com>
2025-01-16 17:48:53 +0100

    training script

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 import torch
 from torch.optim.lr_scheduler import SequentialLR, LinearLR, CosineAnnealingLR
+import matplotlib
+import matplotlib.pyplot as plt
 
+import time
+from statistics import mean
 
 import models
 import data
@@ -13,8 +17,8 @@ config = {
     "BS"         : 4,
     "LR"         : 0.01,
     "WD"         : 0.01,
-    "wup_epochs" : int(12/3),
-    "n_epochs"   : 12,
+    "wup_epochs" : int(6/3),
+    "n_epochs"   : 4,
 }
 torch.manual_seed(config["seed"])
 
@@ -54,13 +58,183 @@ loss = models.Loss(dboxes,scalar=1.0)
 
 print('Starting training...')
 for epoch in range(config["n_epochs"]):
+    beginning = time.perf_counter()
 
     model.train()
+    running_loss = list()
     for step, (images, target_dict) in enumerate(train_loader):
         # send data to gpu (annoying)
         images = images.to(config["device"],non_blocking=True)
 
+        # forward pass
+        plocs, plabel, ptmap = model(images) #plocs.shape(torch.Size([BS, 4, 3024])) and plabel.shape(torch.Size([BS, 1, n_dfboxes]))
+        # encode targets/default boxes
+        gloc, glabel = [], []
+        for i in range(config["BS"]):
+            true_bboxes = target_dict[i]["boxes"].to(config["device"],non_blocking=True) #torch.Size([x, 4])
+            true_labels = target_dict[i]["labels"].to(config["device"],non_blocking=True) #torch.Size([x])
+
+            encoded_gloc, encoded_glabel = encoder.encode(true_bboxes, true_labels) #torch.Size([n_dfboxes, 4]) and torch.Size([n_dfboxes])
+
+            gloc.append(encoded_gloc.to(config["device"],non_blocking=True))
+            glabel.append(encoded_glabel.to(config["device"],non_blocking=True)) 
+
+# ###
+#         f,ax = plt.subplots()
+#         img = images.cpu().detach().numpy()
+#         ii = ax.imshow(img[i][0],cmap='binary_r')
+#         # ax.hlines([-np.pi,np.pi],-3,3,color='red',ls='dashed')
+#         # ax.hlines([-1.9396086193266369,1.940238044375465],-3,3,color='orange',ls='dashed')
+#         for bbx in true_bboxes:
+#             bbx = bbx.cpu().detach().numpy()
+#             bb = matplotlib.patches.Rectangle((bbx[0],bbx[1]),bbx[2]-bbx[0],bbx[3]-bbx[1],lw=1,ec='limegreen',fc='none')
+#             ax.add_patch(bb)
+#         cbar = f.colorbar(ii,ax=ax)
+#         cbar.ax.get_yaxis().labelpad = 10
+#         cbar.set_label('cell significance', rotation=90)
+#         ax.set(xlabel='eta',ylabel='phi')
+#         f.savefig('central-image-example.png')
+#         plt.close()
+#         quit()
+# ###
+
+        gloc = torch.stack(gloc).to(config["device"],non_blocking = True) #torch.Size([BS, n_dfboxes, 4])
+        glabel = torch.stack(glabel).to(config["device"],non_blocking = True)#torch.Size([BS, n_dfboxes])
+        gloc = gloc.permute(0, 2, 1) #torch.Size([BS, 4, 3024])
+
+        train_loss = loss(plocs, plabel, gloc, glabel)
+        running_loss.append(train_loss.item())
+
+        # back prop
+        optimizer.zero_grad()
+        train_loss.backward()
+        optimizer.step()
+
+    print(f"\tEpoch {epoch} / {config['n_epochs']}: train loss {mean(running_loss):.4f}, train time {time.perf_counter() - beginning:.2f}s, LR: {optimizer.param_groups[0]['lr']:.4f}")
+
+
+    # validation step
+    model.eval()
+    with torch.inference_mode():
+        running_val_loss = list()
+        for step, (val_images, val_dict) in enumerate(val_loader):
+            # send data to gpu (annoying)
+            val_images = val_images.to(config["device"],non_blocking=True) 
+        
+            # forward pass
+            plocs, plabel, ptmap = model(val_images) #plocs.shape(torch.Size([8, 4, 8732])) and plabel.shape(torch.Size([8, 1, 8732]))
+
+            # encode val_targets/default boxes
+            gloc, glabel = [], []
+            for i in range(config["BS"]):
+                true_bboxes = val_dict[i]["boxes"].to(config["device"],non_blocking=True) #torch.Size([x, 4])
+                true_labels = val_dict[i]["labels"].to(config["device"],non_blocking=True) #torch.Size([x])
+
+                encoded_gloc, encoded_glabel = encoder.encode(true_bboxes, true_labels) #torch.Size([n_dfboxes, 4]) and torch.Size([n_dfboxes])
+                gloc.append(encoded_gloc.to(config["device"],non_blocking=True))
+                glabel.append(encoded_glabel.to(config["device"],non_blocking=True)) 
+
+            gloc = torch.stack(gloc).to(config["device"],non_blocking = True) #torch.Size([8, 8732, 4])
+            glabel = torch.stack(glabel).to(config["device"],non_blocking = True)#torch.Size([8, 8732])
+            gloc = gloc.permute(0, 2, 1) #torch.Size([8, 4, 8732])
+
+            val_loss = loss(plocs, plabel, gloc, glabel) 
+            running_val_loss.append(val_loss.item())
+        
+    print(f"\tEpoch {epoch} / {config['n_epochs']}: valid loss {mean(running_val_loss):.4f}, valid time {time.perf_counter() - beginning:.2f}s")
+    
+    # update LR scheduler
+    scheduler.step()
+
+
+# save trained model
+model_name = "jetSSD_{}_{}e".format(model.backbone_name,config["n_epochs"])
+print(f'Saving model now...\t{model_name}')
+torch.save(model.state_dict(), "saved_models/{}.pth".format(model_name))
 
 
 
+########################################################################################################################
+
+
+
+print("Finished training. Now let's look at one event and infer")
+print(f"Using default {dboxes.dboxes.shape} boxes and Encoder")
+model.eval()
+with torch.no_grad():
+    for i, (images,targets) in enumerate(test_loader):
+        images = images.to(config["device"]).float()
+        model = model.to(config["device"])
+
+        locs,conf,ptmap = model(images)
+        print("decoding:")
+        # define NMS scriteria, confidence threshold
+        output = encoder.decode_batch(locs, conf, ptmap,
+                                        iou_thresh=0.3, #NMS
+                                        confidence=0.6, #conf threshold
+                                        max_num=150) 
+
+        boxes, labels, scores, pts = zip(*output)
+
+        # examine event by event
+        for j, ((boxes, labels, scores, pts), img) in enumerate(zip(output, images)):
+            print(j)
+            print(min(scores.cpu()))
+            
+            # make truth boxes cover extent
+            extent_j = targets[j]['extent']
+            tru_boxes_ext = targets[j]['boxes'].cpu()
+            tru_boxes_ext[:,(0,2)] = (tru_boxes_ext[:,(0,2)]*((extent_j[1]-extent_j[0])/img.shape[2]))+extent_j[0]
+            tru_boxes_ext[:,(1,3)] = (tru_boxes_ext[:,(1,3)]*((extent_j[3]-extent_j[2])/img.shape[1]))+extent_j[2]
+            
+            tru_pt = targets[j]['jet_pt']
+            event_number = targets[j]['event_no']
+            pred_scores = scores.cpu()
+            pred_pts = pts.cpu()
+            pred_labels = labels.cpu()
+
+            # make pred boxes cover extent
+            det_boxes_ext = boxes.clone().cpu()
+            det_boxes_ext[:,(0,2)] = (det_boxes_ext[:,(0,2)]*((extent_j[1]-extent_j[0])))+extent_j[0]
+            det_boxes_ext[:,(1,3)] = (det_boxes_ext[:,(1,3)]*((extent_j[3]-extent_j[2])))+extent_j[2]
+            print("\tAnti-kt jet pt: ", tru_pt)
+            print("\tPred box jet pt: ", pred_pts)
+
+
+            # plotting
+            MIN_CELLS_PHI,MAX_CELLS_PHI = -3.1334076, 3.134037
+            # MIN_CELLS_ETA,MAX_CELLS_ETA = -4.823496, 4.823496
+            MIN_CELLS_ETA,MAX_CELLS_ETA = -2.5, 2.5
+
+            f,ax = plt.subplots(1,2)
+            ax[0].imshow(img[0].cpu().numpy(),cmap='binary_r',extent=extent_j,origin='lower')
+            ax[1].imshow(img[1].cpu().numpy(),cmap='binary_r',extent=extent_j,origin='lower')
+            
+            ax[0].axhline(y=MIN_CELLS_PHI, color='red', alpha=0.6, linestyle='--',lw=0.7)
+            ax[0].axhline(y=MAX_CELLS_PHI, color='red', alpha=0.6, linestyle='--',lw=0.7)
+            ax[1].axhline(y=MIN_CELLS_PHI, color='red', alpha=0.6, linestyle='--',lw=0.7)
+            ax[1].axhline(y=MAX_CELLS_PHI, color='red', alpha=0.6, linestyle='--',lw=0.7)
+
+            for bbx in tru_boxes_ext:
+                x,y=float(bbx[0]),float(bbx[1])
+                w,h=float(bbx[2])-float(bbx[0]),float(bbx[3])-float(bbx[1])  
+                ax[0].add_patch(matplotlib.patches.Rectangle((x,y),w,h,lw=1,ec='limegreen',fc='none'))
+                ax[1].add_patch(matplotlib.patches.Rectangle((x,y),w,h,lw=1,ec='limegreen',fc='none'))
+
+            for bbx,scr,pt in zip(det_boxes_ext,pred_scores,pred_pts):
+                x,y=float(bbx[0]),float(bbx[1])
+                w,h=float(bbx[2])-float(bbx[0]),float(bbx[3])-float(bbx[1])  
+                ax[0].add_patch(matplotlib.patches.Rectangle((x,y),w,h,lw=1.25,ec='red',fc='none'))
+                ax[1].add_patch(matplotlib.patches.Rectangle((x,y),w,h,lw=1.25,ec='red',fc='none'))
+                ax[1].text(x+w,y+h, f"{scr.item():.2f}",color='red',fontsize=6)
+                ax[1].text(x,y+h/20, f"{pt.item():.0f}",color='red',fontsize=6)
+
+            ax[0].set(xlabel='$\eta$',ylabel='$\phi$')
+            ax[1].set(xlabel='$\eta$',ylabel='$\phi$')
+            plt.tight_layout()
+            model_name = "jetSSD_{}_{}e".format(model.backbone_name,config["n_epochs"])
+            f.savefig('{}-{}-{}.png'.format(model_name,j,event_number))
+            plt.close()
+            
+            quit()
 
