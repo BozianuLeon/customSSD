afe04fd61fd7cf5435f491fc71a3455a514a1f24
bozianuleon <74475015+BozianuLeon@users.noreply.github.com>
2025-01-22 21:18:35 +0100

    new small convnext 49k

--- models/ssd.py
+++ models/ssd.py
@@ -3,10 +3,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 
-from models.convnext import petiteConvNeXt, tinyConvNeXt, UConvNeXt, UConvNeXt_central
+from models.convnext import petiteConvNeXt, tinyConvNeXt, UConvNeXt, UConvNeXt_central, smallConvNeXt_central
 from models.resnext import ResNeXt20
 from models.sumpool import MaskSumPool
 from models.layernorm import LayerNorm2d
+
+# from convnext import petiteConvNeXt, tinyConvNeXt, UConvNeXt, UConvNeXt_central, smallConvNeXt_central
+# from resnext import ResNeXt20
+# from sumpool import MaskSumPool
+# from layernorm import LayerNorm2d
 
 class CustomFeatureExtractor(nn.Module):
     def __init__(self, name, in_channels=3,hidden_channels=24):
@@ -60,6 +65,12 @@ class CustomFeatureExtractor(nn.Module):
         elif name=="uconvnext_central":
             self.feature_extractor = UConvNeXt_central(num_channels=in_channels,hidden_channels=hidden_channels)
             self.out_channels = [hidden_channels*2]
+        
+        elif name=="smallconvnext_central":
+            backbone = smallConvNeXt_central(num_channels=in_channels,hidden_channels=hidden_channels)
+            self.out_channels = [hidden_channels*2] 
+            # print(list(backbone.children())[:8]) # 16
+            self.feature_extractor = torch.nn.Sequential(*list(backbone.children())[:8])
 
     def forward(self, x):
         return self.feature_extractor(x)
@@ -78,8 +89,8 @@ class SSD(torch.nn.Module):
         self._build_additional_features(self.feature_extractor.out_channels)
         self.num_defaults = [1]
         
-        self.loc = nn.Conv2d(self.feature_extractor.out_channels[0], self.num_defaults[0] * 4, kernel_size=3, padding=1)
-        self.conf = nn.Conv2d(self.feature_extractor.out_channels[0], self.num_defaults[0] * self.label_num, kernel_size=3, padding=1)
+        self.loc = nn.Conv2d(self.aux_channels, self.num_defaults[0] * 4, kernel_size=3, padding=1)
+        self.conf = nn.Conv2d(self.aux_channels, self.num_defaults[0] * self.label_num, kernel_size=3, padding=1)
         print(f"Loc head:          {sum(p.numel() for p in self.loc.parameters()):,} parameters.")
         print(f"Conf head:         {sum(p.numel() for p in self.conf.parameters()):,} parameters.")
 
@@ -97,21 +108,21 @@ class SSD(torch.nn.Module):
         
         self._init_weights()
 
-    def _build_additional_features(self, input_size, hidden_channels=24):
-        layer1 = nn.Sequential(
-                    nn.Conv2d(input_size[0], hidden_channels, kernel_size=3, padding=1, stride=1, bias=False),
-                    LayerNorm2d(hidden_channels),
-                    nn.Conv2d(hidden_channels, hidden_channels, kernel_size=1, bias=False),
-                    nn.GELU(),)
+    def _build_additional_features(self, input_size, hidden_channels=12):
         
-        layer2 = nn.Sequential(
-                    nn.Conv2d(hidden_channels, hidden_channels, kernel_size=3, padding=1, stride=1, bias=False),
-                    LayerNorm2d(hidden_channels),
-                    nn.Conv2d(hidden_channels, self.feature_extractor.out_channels[0], kernel_size=1, bias=False),
-                    nn.GELU(),
-                    nn.MaxPool2d(2,padding=(1,0)),)
+        self.aux_channels = int(hidden_channels)
+        aux_layers = nn.Sequential(
+                nn.Conv2d(input_size[0], self.aux_channels, kernel_size=5, padding=2, padding_mode='circular', stride=1, bias=False),
+                LayerNorm2d(self.aux_channels),
+                nn.GELU(),
+                nn.Conv2d(self.aux_channels, self.aux_channels, kernel_size=3, padding=1, padding_mode='circular', stride=1, bias=False),
+                LayerNorm2d(self.aux_channels),
+                nn.Conv2d(self.aux_channels, self.aux_channels, kernel_size=3, padding=(1,0), padding_mode='circular', stride=2, bias=False),
+                nn.SELU(),
+                # nn.MaxPool2d(kernel_size=2,padding=(1,0)),
+        )
 
-        self.additional_blocks = nn.ModuleList([layer1,layer2])
+        self.additional_blocks = nn.ModuleList([aux_layers])
 
         total_params = sum(p.numel() for p in self.additional_blocks.parameters())
         print(f"Aux. layers:       {total_params:,} parameters.")
@@ -144,7 +155,6 @@ class SSD(torch.nn.Module):
         # print('After additional blocks',x.shape)
 
         # print('feature maps [125,96] reshape->',125*96, 'but we downsample the image in aux layers to to reduce to [63,48]',63*48,'equal to our step_x, step_y=2' )
-        # print('I want: Number of dboxes:  torch.Size([3024, 4])')
         locs, confs = self.bbox_view(x, self.loc, self.conf)
         # print('After bbox_view',confs.shape,locs.shape)
 
@@ -157,7 +167,8 @@ if __name__=="__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = SSD(backbone_name="uconvnext_central",in_channels=5)
+    # model = SSD(backbone_name="uconvnext_central",in_channels=5)
+    model = SSD(backbone_name="smallconvnext_central",in_channels=5)
     model = model.to(device) 
     total_params = sum(p.numel() for p in model.parameters())
     print(model.backbone_name,f'!total \t{total_params:,} parameters.\n')
